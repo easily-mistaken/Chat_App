7bb5f0a643ae411ebd7ba6bab6eb688696946bd3
easily-mistaken <choubeyprajjwal@gmail.com>
2024-12-13 13:42:56 +0530

    client side

--- server/index.js
+++ server/index.js
@@ -17,13 +17,14 @@ const expressServer = app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
 
-// State
+// state
 const UsersState = {
   users: [],
   setUsers: function (newUsersArray) {
     this.users = newUsersArray;
   },
 };
+
 const io = new Server(expressServer, {
   cors: {
     origin:
@@ -36,8 +37,9 @@ const io = new Server(expressServer, {
 io.on("connection", (socket) => {
   console.log(`User ${socket.id} connected`);
 
-  // upon connection - only to user
+  // Upon connection - only to user
   socket.emit("message", buildMsg(ADMIN, "Welcome to Chat App!"));
+
   socket.on("enterRoom", ({ name, room }) => {
     // leave previous room
     const prevRoom = getUser(socket.id)?.room;
@@ -48,49 +50,79 @@ io.on("connection", (socket) => {
         "message",
         buildMsg(ADMIN, `${name} has left the room`)
       );
+    }
 
-      const user = activateUser(socket.id, name, room);
+    const user = activateUser(socket.id, name, room);
 
-      // Cannot update previous room users list until after the state update in active user
-      if (prevRoom) {
-        io.to(prevRoom).emit("userList", {
-          users: getUsersInRoom(prevRoom),
-        });
-      }
+    // Cannot update previous room users list until after the state update in activate user
+    if (prevRoom) {
+      io.to(prevRoom).emit("userList", {
+        users: getUsersInRoom(prevRoom),
+      });
+    }
 
-      //join room
-      socket.join(user.room);
+    // join room
+    socket.join(user.room);
 
-      // To user who joined
-      socket.emit(
+    // To user who joined
+    socket.emit(
+      "message",
+      buildMsg(ADMIN, `You have joined the ${user.room} chat room`)
+    );
+
+    // To everyone else
+    socket.broadcast
+      .to(user.room)
+      .emit("message", buildMsg(ADMIN, `${user.name} has joined the room`));
+
+    // Update user list for room
+    io.to(user.room).emit("userList", {
+      users: getUsersInRoom(user.room),
+    });
+
+    // Update rooms list for everyone
+    io.emit("roomList", {
+      rooms: getAllActiveRooms(),
+    });
+  });
+
+  // When user disconnects - to all others
+  socket.on("disconnect", () => {
+    const user = getUser(socket.id);
+    userLeavesApp(socket.id);
+
+    if (user) {
+      io.to(user.room).emit(
         "message",
-        buildMsg(ADMIN, `YOU have joined the ${user.room} chat room`)
+        buildMsg(ADMIN, `${user.name} has left the room`)
       );
 
-      // To everyone else
-      socket.broadcast
-        .to(usere.room)
-        .emit("message", buildMsg(ADMIN, `${user.name} has joined the room`));
+      io.to(user.room).emit("userList", {
+        users: getUsersInRoom(user.room),
+      });
+
+      io.emit("roomList", {
+        rooms: getAllActiveRooms(),
+      });
     }
+
+    console.log(`User ${socket.id} disconnected`);
   });
 
   // Listening for a message event
-  socket.on("message", (data) => {
-    console.log(data);
-    io.emit("message", `${socket.id.substring(0, 5)}: ${data}`);
-  });
-
-  // When user dissconnects - to all others
-  socket.on("disconnect", () => {
-    socket.broadcast.emit(
-      "message",
-      `User ${socket.id.substring(0, 5)} disconnected`
-    );
+  socket.on("message", ({ name, text }) => {
+    const room = getUser(socket.id)?.room;
+    if (room) {
+      io.to(room).emit("message", buildMsg(name, text));
+    }
   });
 
   // Listen for activity
   socket.on("activity", (name) => {
-    socket.broadcast.emit("activity", name);
+    const room = getUser(socket.id)?.room;
+    if (room) {
+      socket.broadcast.to(room).emit("activity", name);
+    }
   });
 });
 
@@ -125,9 +157,9 @@ function getUser(id) {
 }
 
 function getUsersInRoom(room) {
-  return UsersState.users.find((user) => user.room === room);
+  return UsersState.users.filter((user) => user.room === room);
 }
 
 function getAllActiveRooms() {
-  return Array.from(new Set(userState.users.map((user) => user.room)));
+  return Array.from(new Set(UsersState.users.map((user) => user.room)));
 }
